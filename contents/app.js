105516c2862e59e3bcd3e7519f994f1e8da61de5
Prahadeesh K <60143336+prahadeesh189@users.noreply.github.com>
2022-06-09 21:45:50 +0530

    second complete

--- contents/app.js
+++ contents/app.js
@@ -28,51 +28,68 @@ $(document).on('DOMSubtreeModified', async (event) => {
 
             await $(arr[0]).attr('comment-id', commentIndex++ );
             await $(arr[0]).addClass( ''+commentIndex );
-            console.log("----------");
+            // console.log("----------");
 
 
 
 
             await new Promise( async (resolve, reject) => {
-    
+        
+                try {
                 
 
-                await $.ajax({
-                    headers: {        
-                        "Access-Control-Allow-Origin" : "*",
-                        "Access-Control-Allow-Methods":  "GET,HEAD,OPTIONS,POST,PUT",
-                        "Access-Control-Allow-Headers":  "Origin, X-Requested-With, Content-Type, Accept",
-                        "Access-Control-Allow-Private-Network": "true",
-                        "Content-type": "application/json"
-                    },
-                    
-                    type: 'POST',
-                    cache: false,
-                    xhrFields: { withCredentials: true },
-                    url: "http://127.0.0.1:8000/predict",
-                    data: {
-                        "comment_id": commentIndex,
-                        "text": $('.'+commentIndex).text()
-
-                    },
-                    dataType: "text",
-                    success: function(resultData) { 
-                        
-                        console.log( resultData );
-                    
-                    }
-                });
+                    // console.log( $('.'+commentIndex).text() );
 
 
+                    $.ajax({
+                        headers: {        
+                            "Access-Control-Allow-Origin" : "*",
+                            "Access-Control-Allow-Methods":  "GET,HEAD,OPTIONS,POST,PUT",
+                            "Access-Control-Allow-Headers":  "Origin, X-Requested-With, Content-Type, Accept",
+                            "Access-Control-Allow-Private-Network": "true",
+                        },
+                        type: 'POST',
+                        cache: false,
+                        xhrFields: { withCredentials: true },
+                        url: "http://127.0.0.1:8000/predict",
+                        data: JSON.stringify({
+                            "comment_id": commentIndex,
+                            "text": $('.'+commentIndex).text()
+                        }),
+                        dataType : "json",
+                        contentType: "application/json; charset=utf-8",
+                        success: function(resultData) {
 
-                console.log( $('.'+commentIndex).text() );
+                            console.log("----------");
+                            console.log( resultData.comment_id );
+                            console.log( $('.'+resultData.comment_id).text() );
+                            console.log( resultData.offensive );
+
+                            if (resultData.offensive == 1) {
+                                $('.'+resultData.comment_id).html('<span style="color: red;">THIS COMMENT HAS OFFENSIVE CONTENT. SO IT IS BLOCKED BY ACD EXTENSION</span>');
+                            }
+
+                
+                            console.log("----------");
+                        },
+                    });
+
+
+
 
 
                 
 
 
+            } catch (error) {
+                console.error(error);
+            }
                 
-                resolve('ok');
+
+
+
+                
+                resolve("ok");
             })
             .then((data) => {
                 
@@ -80,13 +97,15 @@ $(document).on('DOMSubtreeModified', async (event) => {
     
                 $('.'+commentIndex).addClass( 'verified' );
             })
-    
+
+
+                
     
             
             
         
     
-            console.log("----------");
+            // console.log("----------");
 
         }
 
